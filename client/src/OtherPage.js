73d99b8849a121344656b321aee728233e8286c5
Florin Lilea <florin.lilea94@gmail.com>
2021-02-10 10:41:38 +0100

    plm1

---
+++ client/src/OtherPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+  return (
+    <div>
+      PLM PAge
+      <Link to="/">Go back</Link>
+    </div>
+  );
+};
